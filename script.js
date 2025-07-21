2848d9e404cc93ab7aad213a933e49c35620e9f2
Mashrab2000 <mashrabbekraxmonjonov@gmail.com>
2025-07-21 14:28:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector(".inp")
+let btn = document.querySelector(".btn")
+let table = document.querySelector("table")
+
+btn.addEventListener("click", () => {
+table.innerHTML = ""
+  if (input.value) {
+    fetch(`https://api.nationalize.io/?name=${input.value}`)
+        .then(javob => javob.json())
+        .then(malumot => {
+
+            malumot.country.map((davlat, id) => {
+                let tr = document.createElement("tr")
+                tr.innerHTML = ` <td>${id + 1}</td>
+        <td><img src="https://flagcdn.com/16x12/${davlat.country_id.toLowerCase()}.png" alt="SY flag"> ${davlat.country_id}</td>
+        <td>${davlat.probability.toFixed(3)}%</td>`
+                table.append(tr)
+            }
+            )
+
+
+
+
+        })
+  } 
+})
